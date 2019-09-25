884f0a1f36106bf047b15e57a2f6342fc853a873
syaifuladala <adalasyaiful@gmail.com>
2019-09-26 03:48:42 +0700

    first commit

---
+++ webprofile.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+        <title>Syaiful Adala - Profile</title>
+        <link rel="stylesheet" href="maincss.css" type="text/css">
+    </head>
+    <body>
+        <div class="navbar">
+            <div class="menu">
+                <ul>
+                    <li><a href="webprofile.php">Profile</a></li>
+                    <li><a href="kontak.php">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="container">
+            <center>
+                <div class="header">
+                        <img class="imgheader" src="fotoprofile.jpg">
+                            <h1>Syaiful Adala</h1>
+                            <h4>Informatics Engineering UPN "Veteran" Yogyakarta</h4>
+                </div>
+            </center>
+            <div class="about">
+                <h2>About</h2>
+                <p>Experienced Web Developer with a demonstrated history of working in the information
+                    technology and services industry. Skilled in User Experience (UX), PHP, User Interface
+                    Design, Graphic Design, and English. Strong engineering professional with a Bachelor's
+                    degree focused in Information Technology from Universitas Pembangunan Nasional Veteran Yogyakarta. 
+                </p>
+            </div>
+            <div class="basicinfo">
+                <h2>Basic Information</h2>
+                <table width="100%" border="0" class="tabelbasicinfo" align="center">
+                    <tr>
+                        <td width="40%">Address</td>
+                        <td width="60%">Yogyakarta, Indonesia</td>
+                    </tr>
+                    <tr>
+                        <td width="40%">Place of Birth</td>
+                        <td width="60%">Blora</td>
+                    </tr>
+                    <tr>
+                        <td width="40%">Date of Birth</td>
+                        <td width="60%">7 March 1998</td>
+                    </tr>
+                    <tr>
+                        <td width="40%">Gender</td>
+                        <td width="60%">Male</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="footer">
+                <a href="https://mail.google.com/mail/?view=cm&fs=1&to=adalasyaiful@gmail.com&su=Hai_Syaiful">
+                <img class="imgfooter" src="logo-mail.png"></a>
+                <a href="https://linkedin.com/in/syaifuladala"><img class="imgfooter" src="logo-linkedin.png"></a>
+                <a href="https://github.com/syaifuladala"><img class="imgfooter" src="logo-github.png"></a>
+            </div>
+        </div>
+    </body>
+</html>
